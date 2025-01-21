aa9468416caf03c9daeebf36c6ba8c36f9d3eca1
John Ryan <ryjohn@google.com>
2025-01-21 09:57:07 -0800

    add swiftui app

---
+++ swiftui_app/swiftui_app/swiftui_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_appApp.swift
+//  swiftui_app
+//
+//  Created by John Ryan on 1/21/25.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
